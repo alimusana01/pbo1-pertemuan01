a08b842d651610bf9578c074d5fe3810cf28feaf
alimusana01 <alimusana68@gmail.com>
2021-03-30 19:06:47 +0700

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
- public static void main (string [] args){
-  system.out.println("Hello World !!");
-  
+ public static void main (String [] args){
+  System.out.println("Hello git !!");
+  System.out.println("Hello Bang Al !!");
  }
 }
